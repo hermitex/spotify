0867d343caca752789d248677aca902efb631559
hermitex <mercytiberius@yahoo.com>
2019-09-06 06:10:34 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+ //slide function
+window.addEventListener("load", function () {
+  var test = document.getElementById("test").addEventListener("click", testButton);
+  function testButton() {
+    console.log("clicked");
+  }
+});
